b856f79383daf462db9ab770b2680b3067d6fcd2
Connor Norton <connorn314@gmail.com>
2022-11-14 08:54:22 -0800

    setup csrf protection for dev mode

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,44 @@
-var express = require('express');
+const express = require('express');
 
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
 
-var usersRouter = require('./routes/api/users');
-var bindingsRouter = require('./routes/api/bindings');
+const usersRouter = require('./routes/api/users');
+const bindingsRouter = require('./routes/api/bindings');
+const csrfRouter = require('./routes/api/csrf');
 
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
 
+app.use(
+    csurf({
+        cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
+);
 
 app.use('/api/users', usersRouter);
 app.use('/api/bindings', bindingsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
