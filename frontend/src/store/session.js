dec5feb55202093a0feab97207be4732398f597b
Shuhei Shibahara <Shoehey06@gmail.com>
2022-11-14 14:55:29 -0800

    create login signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,7 +60,7 @@ export const sessionErrorsReducer = (state = nullErrors, action) => {
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
-    case RECEIVE_CURRENT_USER:
+    // case RECEIVE_CURRENT_USER:
     case CLEAR_SESSION_ERRORS:
       return nullErrors;
     default:
