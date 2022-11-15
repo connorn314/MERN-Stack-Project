fdc5ae58593674fd9038c2c7ebbb729abb48f31d
michael <michael_aman92@icloud.com>
2022-11-15 15:14:17 -0800

    add keyboard and profile components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,12 @@ import { Switch } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
-
+import ProfilePage from './components/ProfilePage';
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
 import GameIndex from './components/GamesIndex/GamesIndex';
-
-
+import Keyboard from './components/Keyboard'
 import { getCurrentUser } from './store/session';
 
 function App() {
@@ -28,6 +27,7 @@ function App() {
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
         <AuthRoute exact path="/games" component={GameIndex} />
+        <AuthRoute path="/test" component={ProfilePage}/>
       </Switch>
     </>
   );
