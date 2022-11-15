198ae8ec9a955fb7368b4b6a7cc828800cc37a49
Connor Norton <connorn314@gmail.com>
2022-11-15 15:56:30 -0800

    all backend bindings functionality up and running

--- backend/models/Binding.js
+++ backend/models/Binding.js
@@ -2,14 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bindingSchema = Schema({
-    user: Schema.Types.ObjectId,
-    game: Schema.Types.ObjectId,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+        
+    game: {
+        type: Schema.Types.ObjectId,
+        ref: 'Game'
+    },
     controller: {
         type: String,
-        required: true
+        enum: {
+            values: ["pc", "xbox one", "game cube"],
+            message: 'This controller is not supported by key-wi'
+        }
     },
     keyBinds: {
-        
+        type: Map,
+        of: String // may just want to do Object (mixed schema type, not as specific)
     }
     }, {
     timestamps: true
--- backend/routes/api/bindings.js
+++ backend/routes/api/bindings.js
@@ -1,11 +1,76 @@
+const { json } = require('express');
 const express = require('express');
+
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const Binding = mongoose.model('Binding');
+
+const validateBindingInput = require('../../validations/createBinding')
+
 /* GET bindings listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    message: "GET /api/bindings"
-  });
+router.get('/', async function(req, res, next) {
+  try {
+      const bindings = await Binding.find()
+      return res.json(bindings);
+  }
+  catch(err) {
+      return res.json([]);
+  }
 });
+
+router.post('/new', validateBindingInput, async (req, res, next) => {
+  const newBinding = new Binding({
+    user: req.body.user,
+    game: req.body.game,
+    controller: req.body.controller,
+    keyBinds: req.body.keyBinds
+  })
+  if (newBinding && newBinding.save()){
+    return res.json({newBinding})
+  } else {
+    return res.json()
+  }
+});
+
+router.patch('/:id/edit', validateBindingInput, async (req, res, next) => {
+  try {
+    const updatedBinding = await Binding.findByIdAndUpdate(req.params.id, 
+      {
+        user: req.body.user,
+        game: req.body.game,
+        controller: req.body.controller,
+        keyBinds: req.body.keyBinds
+      },
+      {new: true}
+    );
+    return res.json(updatedBinding)
+  } catch(err) {
+    return res.json([err])
+  }
+})
+
+router.get('/:id', async function(req, res, next) {
+  try {
+      const binding = await Binding.findById(req.params.id);
+      return res.json(binding)
+  }
+  catch(err){
+      return res.json([]);
+  }
+});
+
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const binding = await Binding.findByIdAndDelete(req.params.id);
+    console.log("success")
+    return res.json(binding);
+
+  } catch(err) {
+    return res.json([err])
+  }
+});
+
+
 
 module.exports = router;
