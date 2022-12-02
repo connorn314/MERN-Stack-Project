7c268d8ed17b6332eb85063757f250a5641cfdef
Shuhei Shibahara <Shoehey06@gmail.com>
2022-12-02 16:07:12 +0900

    grid set

--- frontend/src/components/FollowIndexItem/FollowIndexItem.jsx
+++ frontend/src/components/FollowIndexItem/FollowIndexItem.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getOneUser } from "../../store/users";
+import FollowButton from "../FollowButton"
+import './FollowIndexItem.css'
 
 const FollowIndexItem = ({follow}) =>{
+  const dispatch = useDispatch();
+
+ 
 
   return (
     <>
+      <div className="main-individual-follow-container">
+        <div id="follow-item-container">
+          <div id="profile-image-container">
+            <div>Img</div>
+          </div>
+          <div id="follow-username-container">
+            <div>username here</div>
+          </div>
+          <div id="follow-button-container-profile">
+            <FollowButton />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- frontend/src/components/FollowsIndex/FollowsIndex.jsx
+++ frontend/src/components/FollowsIndex/FollowsIndex.jsx
@@ -1,13 +1,14 @@
 import FollowIndexItem from "../FollowIndexItem/FollowIndexItem"
+import './FollowsIndex.css'
 
 const FollowsIndex = ({follows}) => {
 
   return (
     <div id="follows-index-container">
       <div id="follow-index-outline">
-        {follows.map(follow =>{
+        {follows.map(follow => 
           <FollowIndexItem follow={follow} />
-        })}
+        )}
       </div>
     </div>
   )
