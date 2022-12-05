33b2b298356b4d55daff25ed3fe95db4f2133a75
Shuhei Shibahara <Shoehey06@gmail.com>
2022-12-05 10:35:09 +0900

    follow button

--- frontend/src/components/FollowIndexItem/FollowIndexItem.jsx
+++ frontend/src/components/FollowIndexItem/FollowIndexItem.jsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
 import { getOneUser } from "../../store/users";
 import FollowButton from "../FollowButton"
 import './FollowIndexItem.css'
 
 const FollowIndexItem = ({follow}) =>{
   const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user);
+  const author = useSelector(state => state.users[follow.userFollowed])
+
+  useEffect(() => {
+    dispatch(getOneUser(follow.userFollowed))
+  }, [])
+
+  if (author === undefined){
+    return null
+  }
+
 
  
 
@@ -13,14 +24,19 @@ const FollowIndexItem = ({follow}) =>{
     <>
       <div className="main-individual-follow-container">
         <div id="follow-item-container">
-          <div id="profile-image-container">
-            <div>Img</div>
-          </div>
-          <div id="follow-username-container">
-            <div>username here</div>
-          </div>
           <div id="follow-button-container-profile">
-            <FollowButton />
+            <div className="positioning-follow-container">
+              <FollowButton userId={follow.userFollowed}/>
+            </div>
+          </div>
+          <div id="profile-image-container">
+            <div id="actual-mini-thumbnail">
+              <div>Img</div>
+            </div>
+          </div>
+          <div id="follower-detail-container">
+            <div className="user-follow-info1">username:</div>
+            <div className="user-follow-info2">{author.username}</div>
           </div>
         </div>
       </div>
