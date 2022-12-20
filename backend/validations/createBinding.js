9c6420bf51fd65eee7a06b0763a585a3636fb833
Connor Norton <connorn314@gmail.com>
2022-12-19 17:02:25 -0800

    fix home button bugs on xbox controller

--- backend/validations/createBinding.js
+++ backend/validations/createBinding.js
@@ -4,7 +4,7 @@ const handleValidationErrors = require('./handleValidationErrors');
 
 const pcInputs = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0",  "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Return", "Enter", "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight", "ControlLeft", "AltLeft", "MetaLeft", "Space", "MetaRight", "AltRight", "ControlRight", "Escape", "LButton", "RButton", "MButton", "WheelDown", "WheelUp"]
 
-const xboxOneInputs = ["Left stick", "Left stick click", "Left bumper", "Left Trigger", "View button", "Xbox button", "Menu button", "Right bumper", "Directional pad up", "Directional pad down", "Directional pad left", "Directional pad right", "Right Trigger", "Right stick", "Right stick click", "X button", "Y button", "A button", "B button"]
+const xboxOneInputs = ["Left stick", "Left stick click", "Left bumper", "Left Trigger", "View button", "Xbox button", "Menu button", "Right bumper", "Directional pad up", "Directional pad down", "Directional pad left", "Directional pad right", "Right Trigger", "Right stick", "Right stick click", "X button", "Y button", "A button", "B button", "Home button", "Start button", "Right start button"]
 
 const gameCubeInputs = ["Left trigger", "Right trigger", "Z button", "Control stick", "Y button", "X button", "A button", "B button", "Start pause button", "Directional pad up", "Directional pad down", "Directional pad left", "Directional pad right", "C-stick"]
 
