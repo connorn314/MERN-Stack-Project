593e5e5ad3afd6afbd761f717bc4a8fea4f4aab4
Connor Norton <connorn314@gmail.com>
2022-12-24 17:32:27 -0800

    fix connor linkedin link

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
             </div>
             <div className='linkedIn-container'>
                 LinkedIn:
-                <a href="hhttps://www.linkedin.com/in/connor-norton-318b0a19a/" target="_blank"><p className='name-1'>Connor Norton</p></a>
+                <a href="https://www.linkedin.com/in/connor-norton-318b0a19a/" target="_blank"><p className='name-1'>Connor Norton</p></a>
                 <a href="https://www.linkedin.com/in/shuhei-shibahara/" target="_blank"><p className='name-2'>Shuhei Shibahara</p></a>
                 <a href="https://www.linkedin.com/in/michael-aman-ba1086258/" target="_blank"><p className='name-3'>Michael Aman</p></a>
             </div>
